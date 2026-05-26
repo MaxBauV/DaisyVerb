94628e8a45f7986e20c735571eeb73b9f5f388a0
Maximilian Bauer <maximilian.bauer@akkodis.com>
2026-05-26 16:44:25 +0200

    SDRAM-stuff now really passed as ref

--- src/VersioReverb.h
+++ src/VersioReverb.h
@@ -39,14 +39,14 @@ class VersioReverb
 public:
 
 	VersioReverb(
-		daisysp::DelayLine<float, DELAYLINE1> del_0_l,
-		daisysp::DelayLine<float, DELAYLINE2> del_1_l,
-		daisysp::DelayLine<float, DELAYLINE3> del_2_l,
-		daisysp::DelayLine<float, DELAYLINE4> del_3_l,
-		daisysp::DelayLine<float, DELAYLINE1> del_0_r,
-		daisysp::DelayLine<float, DELAYLINE2> del_1_r,
-		daisysp::DelayLine<float, DELAYLINE3> del_2_r,
-		daisysp::DelayLine<float, DELAYLINE4> del_3_r,
+		daisysp::DelayLine<float, DELAYLINE1> &del_0_l,
+		daisysp::DelayLine<float, DELAYLINE2> &del_1_l,
+		daisysp::DelayLine<float, DELAYLINE3> &del_2_l,
+		daisysp::DelayLine<float, DELAYLINE4> &del_3_l,
+		daisysp::DelayLine<float, DELAYLINE1> &del_0_r,
+		daisysp::DelayLine<float, DELAYLINE2> &del_1_r,
+		daisysp::DelayLine<float, DELAYLINE3> &del_2_r,
+		daisysp::DelayLine<float, DELAYLINE4> &del_3_r,
 		daisysp::PitchShifter &ps_l,
 		daisysp::PitchShifter &ps_r
 	);
